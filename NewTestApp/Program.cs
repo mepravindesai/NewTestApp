5c82fe1cb76e4cbe2408d8829a173c39d1cd5697
Admin <Admin@DESKTOP-B826H16>
2026-01-06 22:38:23 +0530

    Add project files.

---
+++ NewTestApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, sum;
+            Console.Write("Enter first num:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second num:");
+            b = Convert.ToInt32(Console.ReadLine());
+            sum = a + b;
+            Console.Write("Final Sum :");
+            Console.WriteLine(sum);
+            Console.WriteLine(new Refer());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ NewTestApp/Refer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace NewTestApp
+{
+    internal class Refer
+    {
+        public Refer()
+        {
+            Console.WriteLine("see the message from another class:");
+        }
+    }
+}
