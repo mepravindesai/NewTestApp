b47c2b8c42a1ed072a63a3954a9e191fa9f20bcf
mepravindesai <pd29desai@gmail.com>
2026-01-07 11:52:27 +0530

    New changes

---
+++ NewTestApp/testing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTestApp
+{
+    internal class testing
+    {
+        public void viewMSG()
+        {
+            int a;
+            float b;
+        }
+    }
+}
